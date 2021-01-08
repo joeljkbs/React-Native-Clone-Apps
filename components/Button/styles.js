bce010017ee552267a72815617d071401132bce7
joeljkbs <joeljacksebastian@gmail.com>
2021-01-08 16:49:06 -0500

    Buttons added

---
+++ components/Button/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {        
+        height: 40,
+        borderRadius: 20,
+        width: '96%',
+        marginLeft:6,
+        padding: 10
+    },
+    button: {        
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    text: {
+        fontSize: 12,
+        fontWeight: '500',
+        textTransform: 'uppercase'
+    }
+});
+
+export default styles;
--- components/CarModel/index.js
+++ components/CarModel/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text,ImageBackground} from  'react-native';
+import Button from '../Button/index';
 import styles from './styles';
 
 const CarModel= (props) => {
@@ -10,11 +11,27 @@ return (
       source={require('../../assets/images/ModelX.jpeg')}
       style={styles.image}>
     </ImageBackground>
-
       <View style={styles.titles}>
         <Text style={styles.title1}>Model S</Text>
         <Text style={styles.title2}>Starting at C$69,420</Text>
       </View>
+
+      <Button 
+        type="primary" 
+        content={"Custom Order"} 
+        onPress={()=>{
+          console.warn("Custom Order was pressed");
+        }}
+      />
+
+      <Button 
+        type="secondary" 
+        content={"Existing Inventory"} 
+        onPress={()=>{
+          console.warn("Existing inventory was pressed");
+        }}
+      />
+      
     </View>
 );
 };
